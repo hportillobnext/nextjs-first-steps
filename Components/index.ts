76d8809ec92d6db911a5719780abacf01517ac02
hportillobnext <hportillo@bnext.mx>
2024-08-26 16:28:13 -0600

    algunos cambios y agregar pagina about que faltaba

--- Components/index.ts
+++ Components/index.ts
@@ -1,4 +1,7 @@
 import NavBar from "./navBar/NavBar";
+
+
+//Client Components
 import ActiveLink from "./navBar/active-link/ActiveLink";
 
 export {
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <main className="flex flex-col items-center p-24">
-        <span className="text-7xl">Hola Mundo!! </span>
+        <span className="text-7xl">Hola Mundo!!! </span>
 
       </main>
     </>
